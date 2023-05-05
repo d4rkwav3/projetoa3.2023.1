f5d8c2bde1fa92c338087c8a78f700aaa8ba9b57
Bruno Venâncio de Souza e Silva <brunosilva.5934@aluno.saojudas.br>
2023-05-04 21:35:26 -0300

    Construindo função de validar email

--- Android/app/src/main/java/com/usjt/projetoa3/model/EmailValidation.kt
+++ Android/app/src/main/java/com/usjt/projetoa3/model/EmailValidation.kt
@@ -4,18 +4,37 @@ class EmailValidation : Validation() {
     val MIN_EMAIL_LENGTH = 10
     val MIN_USERNAME_LENGTH = 3
     val MIN_DOMAIN_LENGTH = 5
-    val MAX_EMAIL_LENGTH = 200
+    val MAX_EMAIL_LENGTH = 128
     val EXPECTED_CHARS = arrayOf('@', '.')
 
     fun validateEmail(email: String): Boolean {
-        var atSign = email.contains(this.EXPECTED_CHARS[0])
-        var period = email.contains(this.EXPECTED_CHARS[1])
-        var emailLength = when {
-            email.length < this.MIN_EMAIL_LENGTH -> false
-            email.length >= this.MAX_EMAIL_LENGTH -> false
+        val atSign = email.contains(this.EXPECTED_CHARS[0])
+        val period = email.contains(this.EXPECTED_CHARS[1])
+        val emailLength = when {
+            email.length < 10 -> false
+            email.length >= 200 -> false
             else -> true
         }
+        var username: String = ""
+        var domain: String = ""
+        var validUsername = false
+        var validDomain = false
 
-        return atSign && period && emailLength
+        if(atSign) {
+            for((index, char) in email.withIndex()) {
+                if(char == '@') {
+                    username += email.substring(startIndex = 0, endIndex = index)
+                    domain += email.substring(startIndex = index + 1, endIndex = email.length)
+                }
+            }
+
+            validDomain = domain.contains(this.EXPECTED_CHARS[1])
+        }
+
+
+        println(username)
+        println(domain)
+
+        return atSign && period && emailLength && username.length >= 3 && domain.length >= 5
     }
 }
---
+++ Android/app/src/test/java/com/usjt/projetoa3/ValidationTest.kt
@@ -0,0 +1,15 @@
+package com.usjt.projetoa3
+
+import com.usjt.projetoa3.model.Validation
+import org.junit.Test
+
+class ValidationTest() {
+
+//    @Test
+//    fun checkForInvalidChar() {
+//        val validation = Validation()
+//
+//        assert(false) { validation.checkForInvalidChar("bruno*venancio") }
+//        assert(true) { validation.checkForInvalidChar("bruno.venancio") }
+//    }
+}
